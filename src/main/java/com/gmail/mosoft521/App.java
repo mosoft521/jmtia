20a070652bd8c903f1c2123f16322115e0cdd735
Alvin Zhang <mosoft521@gmail.com>
2017-08-19 17:22:54 +0800

    format

--- src/main/java/com/gmail/mosoft521/App.java
+++ src/main/java/com/gmail/mosoft521/App.java
@@ -2,12 +2,9 @@ package com.gmail.mosoft521;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
     }
 }
